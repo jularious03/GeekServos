409ca45f71815c6589dc227015bdf023b304077c
jularious03 <SchmidleitnerJ23060@sus.htl-grieskirchen.at>
2026-07-11 08:48:12 +0200

    Fix structure for MakeCode

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
 //% color="FFC0CB" icon="\u489" block="InnRaum"
-namespace InnRaum{
-    /**
-     * Shows a greeting on the LED-Pad
-     */
-    //% block="show gretting"
-    export function showMessage(): void {
-        basic.showString("hello world");
-    }
 
-    //% block="wait for %ms ms"
-    export function waitMs (ms: number) : void {
-        basic.pause(ms);
-    }
+namespace InnRaum {
+  /**
+   * Shows a greeting on the LED-Pad
+   */
+  //% block="show gretting"
+  export function showMessage(): void {
+    basic.showString('hello world');
+  }
+
+  //% block="wait for %ms ms"
+  export function waitMs(ms: number): void {
+    basic.pause(ms);
+  }
 }
